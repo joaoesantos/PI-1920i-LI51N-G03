772f1af16b94188f81e9d4f4420b3492b3530db6
LAPTOP-24HRLEIC\Quintela <aquintelaoliveira@gmail.com>
2019-10-30 18:36:46 +0000

    router and router-tests update

--- libs/CIBORG/webapi/router.js
+++ libs/CIBORG/webapi/router.js
@@ -3,80 +3,106 @@
 // const webapi = require('./ciborg-web-api.js'); é necessario?
 
 let router = function (request, response) {
-   
     //nativate(request,response);
 };
 
 // registered route templates
 router.routes = [];
 
-router.nativate = function(req, res) {
-    // find command by matching url with route templates
-    if(routes.length != 0) {
-        routes.forEach(function(route) {
-            // find params and replace them with regex
-            let template = route.url.replace(/:\w+/g, `([^/]+)`);
-            // convert to regex and only match from start to end
-            template = new RegExp(`^${template}$`);
-            // matchObj will be null is there is no match
-            let matchObj = _url.match(template);
-            if(matchObj != null) {
-                // add parameter to request
-                let params =  createParameterObject(route, matchObj);
-                req['body'] = params;
-            }
-            //matchObj.service;
-            console.log(req);
-        });
-    } else {
-        //res.statusCode = '404';
+function processRequest(req, rsp) {
+
+    console.log(req.method)
+    console.log(req.url)
+    console.log(req.headers)
+
+    let data = ""
+
+    req.on('data', chunck => data += chunck.toString());
+    req.on('end', processBodyAndResponse);
+
+    function processBodyAndResponse() {
+        console.log('Received : ', data)
+        res.setHeader('Content-Type','text/plain')
+        res.end('Received and Done')
     }
+    
+}
+
+// navigate url and calls web-api services if exists
+router.navigate = function(req, rsp) {
+    // find command by matching url with route templates
+    if(router.routes.length == 0) {
+        rsp.statusCode = '404';
+        rsp.statusMessage = 'There is no routes implemented.';
+        //res.end(); 
+        return;
+    }
+    // matches url with templates
+    router.routes.some(function(route) {
+        // finds parameters and replace them with regex
+        let template = route.template.replace(/:\w+/g, `([^/]+)`);
+        // convert to regex and only match from start to end
+        template = new RegExp(`^${template}$`);
+        // matchObj will be null is there's no match
+        let matchObj = req.url.match(template);
+        if(matchObj != null) {
+            // add parameters to request if there's any
+            addParametersToRequest(req, route, matchObj);
+            // call web-api
+            route.webapi;
+            rsp.statusMessage = route.webapi;
+            return true;
+        } else {
+            rsp.statusCode = '404';
+            rsp.statusMessage = 'Command ' + req.url + ' does not exist.';
+            //res.end(); 
+        }
+    });
+
 };
 
-// add parameteres to request
-function createParameterObject(route, matchObj) {
-    let paramObj = {};
-    paramObj['url'] = route.url;
-    paramObj['service'] = route.service;
-    let params = route.url.match(/:\w+/g);
+// add parameteres to request body
+function addParametersToRequest(req, route, matchObj) {
+    let body = {};
+    let params = route.template.match(/:\w+/g);
     if(params != null) { // has more parameters
         for(let i = 0; i <params.length; i++) {
-            paramObj[params[i].substring(1)] = matchObj[i+1];
+            body[params[i].substring(1)] = matchObj[i+1];
         }
+        req['body'] = body;
     }
-    return paramObj;
 }
 
 // check for duplicated routes
-function hasRoute(_url) {
+function hasRoute(url) {
     let hasDuplicate = false;
     router.routes.forEach(function(route) {
-    if(_url === route.url)
+    if(url === route.url)
         hasDuplicate = true;
     });
     return hasDuplicate;  
 };
 
-// add command template to routes and
-function add(url, service) {
-    if(!hasRoute(url))
-        router.routes.push({ url : url, service : service });
+// add new template to routes
+function add(template, webapi) {
+    if(!hasRoute(template))
+        router.routes.push({ template : template, webapi : webapi });
 };
-router.get = function(req, service) { 
-    add('GET ' + req.url, service);
-    //req.method = 'GET';
+router.get = function(template, webapi) { 
+    add('GET ' + template, webapi);
+    return 'Received a GET HTTP method';
 };
-router.post = function(req, service) {        
-    add('POST ' + req.url, service);
-    //req.method = 'POST';
+router.post = function(template, webapi) {        
+    add('POST ' + template, webapi);
+    return 'Received a POST HTTP method';
 };
-router.put = function(req, service) {
-    add('PUT ' + req.url, service);
-    //req.method = 'PUT';
+router.put = function(template, webapi) {
+    add('PUT ' + template, webapi);
+    //return res.send('Received a PUT HTTP method');
 };
-router.delete = function(req, service) {
-    add('DELETE ' + req.url, service);
-    //req.method = 'DELETE'
+router.delete = function(template, webapi) {
+    add('DELETE ' + template, webapi);
+    return 'Received a DELETE HTTP method';
 };
 
 module.exports = router;
