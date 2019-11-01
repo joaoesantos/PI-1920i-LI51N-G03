5441080c06da1af1e860f12650bbe829c669def1
jerss <joao.silva.santos1988@gmail.com>
2019-11-01 10:27:43 +0000

    Corrected GameDto e GameDtoMapper

--- libs/CIBORG/services/games/ciborg-services-games.js
+++ libs/CIBORG/services/games/ciborg-services-games.js
@@ -1,8 +1,9 @@
 'use strict';
 const request = require('request');   
 //const config = require('../../shared/Config.js');
-let GameDto = require('../../entities/dtos/GameDto.js');
-let gameMapper = require('../../entities/mappers/GameDtoMapper.js');
+const client_id = 'rFMyVCTRWP';
+const GameDto = require('../../entities/dtos/GameDto.js');
+const GameMapper = require('../../entities/mappers/GameDtoMapper.js');
 
 
 function createHttpRequest(options,resolved, rejected){
@@ -39,7 +40,7 @@ function searchGamesByGroup(id, cb){
 function searchByName(gameName, cb){
     // Setting URL and headers for request
     var options = {
-        url: `https://www.boardgameatlas.com/api/search?name=${gameName}&client_id=rFMyVCTRWP`,
+        url: `https://www.boardgameatlas.com/api/search?name=${gameName}&client_id=${client_id}`,
         headers: {
             'User-Agent': 'request'
         }
@@ -50,23 +51,23 @@ function searchByName(gameName, cb){
         let arr = data.games.map(function(g) {
 
         let dto = new GameDto(
-                    g.id, 
-                    g.name,
-                    g.year_published, 
-                    g.min_players, 
-                    g.max_players, 
-                    g.min_playtime, 
-                    g.max_playtime, 
-                    g.min_age, 
-                    g.description,
-                    g.description_preview,
-                    g.price,
-                    g.primary_publisher,
-                    g.num_user_ratings,
-                    g.average_user_rating
-                    );
-            return gameMapper.entityToModel(dto);
-        })
+            g.id,
+            g.name,
+            g.year_published,
+            g.min_players,
+            g.max_players,
+            g.min_playtime,
+            g.max_playtime,
+            g.min_age,
+            g.description,
+            g.description_preview,
+            g.price,
+            g.primary_publisher,
+            g.num_user_ratings,
+            g.average_user_rating,
+            );
+            return new GameMapper().entityToModel(dto);
+        });
 
         
         cb(arr);
