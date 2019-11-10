327c5877c55156fdd6c0c5b97905a6fb4baa39be
LAPTOP-24HRLEIC\Quintela <aquintelaoliveira@gmail.com>
2019-11-10 11:35:19 +0000

    refactoring of game board data to use http call with one cb

--- libs/CIBORG/dal/board-games-data.js
+++ libs/CIBORG/dal/board-games-data.js
@@ -1,5 +1,4 @@
 'use strict';
-const request = require('request');   
 const client_id = 'rFMyVCTRWP';
 module.exports = function(GamesDto, GamesDtoMapper, HttpCall, CiborgError){
 
@@ -14,124 +13,104 @@ module.exports = function(GamesDto, GamesDtoMapper, HttpCall, CiborgError){
         return options;
     }
 
-    function getMostPopularGames(cb){
+    function getMostPopularGames(cb) {
         // Setting URL and headers for request
-        var options = {
+        let options = {
             url: `https://www.boardgameatlas.com/api/search?client_id=${client_id}&order_by=popularity&ascending=false`,
             headers: {
                 'User-Agent': 'request'
             }
         };
-    
-        function resolved(data){
-            try{
-                let games = data.games.map(function(g) {
-    
-                    let dto = GamesDto(
-                        g.id,
-                        g.name,
-                        g.year_published,
-                        g.min_players,
-                        g.max_players,
-                        g.min_playtime,
-                        g.max_playtime,
-                        g.min_age,
-                        g.description,
-                        g.description_preview,
-                        g.price,
-                        g.primary_publisher,
-                        g.num_user_ratings,
-                        g.average_user_rating,
-                        );
-    
-                        return GamesDtoMapper.entityToModel(dto);
+        function handler(err, data) {
+            try {
+                if(err) {
+                    cb(err);
+                } else {
+                    let games = data.games.map(function(g) {
+        
+                        let dto = GamesDto(
+                            g.id,
+                            g.name,
+                            g.year_published,
+                            g.min_players,
+                            g.max_players,
+                            g.min_playtime,
+                            g.max_playtime,
+                            g.min_age,
+                            g.description,
+                            g.description_preview,
+                            g.price,
+                            g.primary_publisher,
+                            g.num_user_ratings,
+                            g.average_user_rating,
+                            );
+                            return GamesDtoMapper.entityToModel(dto);
                     });
-
-                    cb(null,{
+                    cb(null, {
                         statusCode: 201,
                         statusMessage: "accepted",
                         body: {games}
                         });
-            }catch(e){
+                }
+            } catch(err) {
                 cb(
                     new CiborgError(
                     'Error calling external service: getMostPopularGames.',
                     'Unable to get popular games.',
-                    '500'));
+                    '500' //Service Unavailable
+                ));
             }
-            
-
-            
-        };
-    
-        function rejected(err){
-            cb(new CiborgError(
-                'Error in service: getAllGames.',
-                'Unable to get popular games.',
-                '503' //Service Unavailable
-            ));
-        };
-
-        return HttpCall.get(options, resolved, rejected);
-    };
+        }
+        HttpCall.get(options, handler);
+    }
 
     function getGameByID(id, cb){
-    
         let options = {
             url: `https://www.boardgameatlas.com/api/search?ids=${id}&client_id=${client_id}`,
             headers: {
                 'User-Agent': 'request'
             }
         };
-    
-        function resolved(data){
-            try{
-                let games = data.games.map(function(g) {
-    
-                    let dto = GamesDto(
-                        g.id,
-                        g.name,
-                        g.year_published,
-                        g.min_players,
-                        g.max_players,
-                        g.min_playtime,
-                        g.max_playtime,
-                        g.min_age,
-                        g.description,
-                        g.description_preview,
-                        g.price,
-                        g.primary_publisher,
-                        g.num_user_ratings,
-                        g.average_user_rating,
-                        );
-    
-                        return GamesDtoMapper.entityToModel(dto);
+        function handler(err, data){
+            try {
+                if(err) {
+                    cb(err);
+                } else {
+                    let games = data.games.map(function(g) {
+        
+                        let dto = GamesDto(
+                            g.id,
+                            g.name,
+                            g.year_published,
+                            g.min_players,
+                            g.max_players,
+                            g.min_playtime,
+                            g.max_playtime,
+                            g.min_age,
+                            g.description,
+                            g.description_preview,
+                            g.price,
+                            g.primary_publisher,
+                            g.num_user_ratings,
+                            g.average_user_rating,
+                            );
+                            return GamesDtoMapper.entityToModel(dto);
                     });
-    
-                cb(null,{
-                    statusCode: 201,
-                    statusMessage: "accepted",
-                    body: {games}
-                    });
-            }catch(e){
+                    cb(null,{
+                        statusCode: 201,
+                        statusMessage: "accepted",
+                        body: {games}
+                        });
+                }
+            } catch(err){
                 cb(new CiborgError(
                     'Error calling external service: getGameByID.',
                     'Unable to get game.',
                     '500' //Service Unavailable
                 ));
-            }
-            
+            } 
         };
-    
-        function rejected(err){
-            cb(new CiborgError(
-                'Error in service: getGameByID.',
-                'Unable to get game.',
-                '503' //Service Unavailable
-            ));
-        };
-    
-        return HttpCall.get(options, resolved, rejected);
+        HttpCall.get(options, handler);
     }
 
     function searchByName(gameName, cb){
@@ -141,56 +120,45 @@ module.exports = function(GamesDto, GamesDtoMapper, HttpCall, CiborgError){
                 'User-Agent': 'request'
             }
         };
-    
-        function resolved(data){
-            try{
-                let games = data.games.map(function(g) {
-    
-                    let dto = GamesDto(
-                        g.id,
-                        g.name,
-                        g.year_published,
-                        g.min_players,
-                        g.max_players,
-                        g.min_playtime,
-                        g.max_playtime,
-                        g.min_age,
-                        g.description,
-                        g.description_preview,
-                        g.price,
-                        g.primary_publisher,
-                        g.num_user_ratings,
-                        g.average_user_rating,
-                        );
-            
-                        return GamesDtoMapper.entityToModel(dto);
+        function handler(err, data) {
+            try {
+                if(err) {
+                    cb(err);
+                } else {
+                    let games = data.games.map(function(g) {
+                        let dto = GamesDto(
+                            g.id,
+                            g.name,
+                            g.year_published,
+                            g.min_players,
+                            g.max_players,
+                            g.min_playtime,
+                            g.max_playtime,
+                            g.min_age,
+                            g.description,
+                            g.description_preview,
+                            g.price,
+                            g.primary_publisher,
+                            g.num_user_ratings,
+                            g.average_user_rating,
+                            );
+                            return GamesDtoMapper.entityToModel(dto);
+                });
+                cb(null,{
+                    statusCode: 201,
+                    statusMessage: "accepted",
+                    body: {games}
                     });
-            
-                    
-                    cb(null,{
-                        statusCode: 201,
-                        statusMessage: "accepted",
-                        body: {games}
-                        });
-            }catch(e){
+                }
+            } catch(err) {
                 cb(new CiborgError(
                     'Error calling external service:: searchByName.',
                     'Unable to search games.',
                     '500' //Service Unavailable
                 ));
-            }
-            
+            }  
         };
-    
-        function rejected(err){
-            cb(new CiborgError(
-                'Error in service: searchByName.',
-                'Unable to get popular games.',
-                '503' //Service Unavailable
-            ));
-        };
-    
-        return HttpCall.get(options, resolved,rejected);
+        HttpCall.get(options, handler);
     };
 
     return {
@@ -198,4 +166,5 @@ module.exports = function(GamesDto, GamesDtoMapper, HttpCall, CiborgError){
         getMostPopularGames: getMostPopularGames,
         getGameByID: getGameByID,
     }
+    
 };
