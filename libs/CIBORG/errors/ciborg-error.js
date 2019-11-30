770753753c6a51ff5098c61b48a8dcc4ab59dfe3
LAPTOP-6TDP5TMD\rodri <rodrigomfl@hotmail.com>
2019-11-30 11:52:51 +0000

    ciborg errors now have stack and custom tostring

--- libs/CIBORG/errors/ciborg-error.js
+++ libs/CIBORG/errors/ciborg-error.js
@@ -1,14 +1,25 @@
 class CiborgError extends Error {
-    constructor(apiErrorMessage, clientErrorMessage, statusCode){
+    constructor(apiErrorMessage, clientErrorMessage, statusCode) {
         super(apiErrorMessage);
         this.clientErrorMessage = clientErrorMessage;
         this.statusCode = statusCode;
+
+        //Creates a stack for this error
+        Error.captureStackTrace(this, this.constructor);
     }
     resolveErrorResponse(rsp) {
         rsp.statusCode = this.statusCode;
         let errorResponsePayload = { payload : { clientErrorMessage : this.clientErrorMessage } };
         errorResponsePayload.payload.apiErrorMessage = this.message;
         rsp.end(JSON.stringify(errorResponsePayload));
+    }
+
+    toString() {
+        return JSON.stringify({
+            apiErrorMessage : this.message,
+            clientErrorMessage : this.clientErrorMessage,
+            statusCode : this.statusCode
+        });
     }
 }
 module.exports =  CiborgError;
